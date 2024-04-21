fe8f68dcad65f4ccee192f3a124c5ba437fa1921
linear-digital <tamizuddin.tr@gmail.com>
2024-04-21 11:22:36 +0600

    update Search_Bar.jsx and Sidebar.jsx

--- src/Pages/Drive/Navbar/Search_Bar.jsx
+++ src/Pages/Drive/Navbar/Search_Bar.jsx
@@ -5,14 +5,14 @@ import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 const Search_Bar = () => {
    
     return (
-        <div contextMenu={false} className='w-[721.60px] h-[47.60px] bg-white rounded-2xl overflow-hidden flex items-center pl-3'>
+        <div contextMenu={false} className='w-[721.60px] h-[47.60px] bg-white rounded-2xl overflow-hidden flex items-center pl-3 shadow'>
             <FontAwesomeIcon icon={faMagnifyingGlass} height={14} />
             <input type="text"
                 className='w-full h-full pl-3 outline-none bg-white'
                 placeholder='Search in drive'
             />
             <button onClick={(e) => {
-            }} className='btn btn-primary rounded-none'>Search</button>
+            }} className='btn hover:shadow btn-primary rounded-none'>Search</button>
         </div>
     );
 };
--- src/Pages/Drive/Sidebar.jsx
+++ src/Pages/Drive/Sidebar.jsx
@@ -9,6 +9,11 @@ import { faClockRotateLeft } from '@fortawesome/free-solid-svg-icons';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { Dropdown, Menu } from 'antd';
 import { faUpload } from '@fortawesome/free-solid-svg-icons';
+import { faFolderClosed } from '@fortawesome/free-regular-svg-icons';
+import { faImage } from '@fortawesome/free-regular-svg-icons';
+import { useState } from 'react';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
 
 const items = [
     {
@@ -25,7 +30,17 @@ const items = [
 const links = [
     {
         name: "All Files",
-        icon: faFolderOpen
+        icon: faFolderOpen,
+        children: [
+            {
+                name: "Images",
+                icon: faImage
+            },
+            {
+                name: "Documents",
+                icon: faFolderClosed
+            }
+        ]
     },
     {
         name: "Shared with me",
@@ -56,6 +71,7 @@ const Sidebar = () => {
             createFolder()
         }
     }
+    const [open, setOpen] = useState('')
     return (
         <div className='w-[350px] bg-gray-50 h-full py-9'>
             <div className="flex justify-center">
@@ -86,13 +102,43 @@ const Sidebar = () => {
             <ul className='mt-6'>
                 {
                     links.map((link, index) => (
-                        <li key={index} className='pl-10 hover:bg-gray-100 '>
-                            <Link to="/drive" className='flex items-center gap-5 font-normal text-[17px] py-3'>
-                                <FontAwesomeIcon icon={link.icon} width={20} height={20} />
-                                <span>
-                                    {link.name}
-                                </span>
-                            </Link>
+                        <li key={index} className=''>
+                            <div className='flex items-center pr-5'>
+                                <Link to="/drive" className='flex items-center gap-5 font-normal text-[17px] py-3 hover:bg-gray-100 w-full pl-10'>
+                                    <FontAwesomeIcon icon={link.icon} width={20} height={20} />
+                                    <span>
+                                        {link.name}
+                                    </span>
+                                </Link>
+                                {
+                                    link.children && <button className='pl-4'
+                                    onClick={() => setOpen(open === index ? '' : index)}
+                                    >
+                                       {
+                                           open === index ? <FontAwesomeIcon icon={faChevronUp} width={20} height={20} /> : <FontAwesomeIcon icon={faChevronDown} width={20} height={20} />
+                                       }
+                                    </button>
+                                }
+                            </div>
+
+                            {
+                                link.children && open === index && (
+                                    <ul>
+                                        {
+                                            link.children.map((child, index) => (
+                                                <li key={index} className='pl-14 hover:bg-gray-100 '>
+                                                    <Link to="/drive" className='flex items-center gap-5 font-normal text-[17px] py-3'>
+                                                        <FontAwesomeIcon icon={child.icon} width={20} height={20} />
+                                                        <span>
+                                                            {child.name}
+                                                        </span>
+                                                    </Link>
+                                                </li>
+                                            ))
+                                        }
+                                    </ul>
+                                )
+                            }
                         </li>
                     ))
                 }
