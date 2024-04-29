baaefb8c50826d497d1b1877248e5223992896b8
linear-digital <tamizuddin.tr@gmail.com>
2024-04-30 00:01:12 +0600

    update Loader.jsx, Router.jsx and authChecker.jsx

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { LoadingOutlined } from '@ant-design/icons';
 const Loader = () => {
     return (
-        <div>
+        <div className='w-full flex justify-center p-5'>
             <Spin indicator={<LoadingOutlined style={{ fontSize: 34, color: '#F7931E' }} spin />} />
         </div>
     );
--- src/helper/authChecker.jsx
+++ src/helper/authChecker.jsx
@@ -3,15 +3,20 @@ import { useNavigate } from 'react-router-dom';
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { useEffect } from 'react';
+import Login from '../Pages/Auth/Login';
+import Loader from '../Components/Loader';
 
 const AuthChecker = ({ children }) => {
     const { user } = useSelector(state => state.user)
     const navigate = useNavigate()
-    if (user) {
+    if (user === null) {
+        return <Loader />
+    }
+    else if (user) {
         return children
     }
     else if (!user) {
-        navigate('/login')
+        return <Login />
     }
 };
 
