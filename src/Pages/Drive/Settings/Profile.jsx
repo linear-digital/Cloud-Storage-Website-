e0da5e0b3567763ae34a5ed9d57dcd3d79beefd2
linear-digital <tamizuddin.tr@gmail.com>
2024-05-02 17:05:01 +0600

    Name Change

--- src/Pages/Drive/Settings/Profile.jsx
+++ src/Pages/Drive/Settings/Profile.jsx
@@ -1,12 +1,39 @@
+import { Button } from '@material-tailwind/react';
 import { Input } from '@material-tailwind/react';
 import React from 'react';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { api } from '../../../Components/axios/api';
+import { useDispatch } from 'react-redux';
+import { setReloadUser } from '../../../redux/Slice/reloadSlice';
+import { toast } from 'react-hot-toast';
 
 const Profile = () => {
+    const { user } = useSelector((state) => state.user)
+    const dispatch = useDispatch()
+    const formHandeler = async (e) => {
+        e.preventDefault()
+        const target = new FormData(e.target)
+        const data = Object.fromEntries(target.entries())
+        try {
+            const res = await api.put(`/user/${user?._id}`, data)
+            dispatch(setReloadUser(res.data))
+            toast.success("Name Changed Successfully")
+        } catch (error) {
+            toast.error(error?.response?.data?.message || error.message || "Something went wrong")
+        }
+    }
     return (
         <div>
             <h1 className='text-lg'>Update name and profile image</h1>
-            <form action="" className='max-w-96 mt-5'>
-                <Input label="Name" />
+            <form onSubmit={formHandeler} className='max-w-96 mt-5'>
+                <Input label="Name"
+                    defaultValue={user?.name}
+                    name='name'
+                />
+                <Button className='mt-3' type='submit'>
+                    Update
+                </Button>
             </form>
         </div>
     );
